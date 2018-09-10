32db58dd0b18930715a84b326f204332b439e047
18290138 <18290138@facin.portoalegre.pucrsnet.br>
2018-09-10 11:44:38 -0300

    Pipeline array

--- pipeline.c
+++ pipeline.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mpi.h"
 
 int main(int argc, char** argv) {
@@ -11,26 +12,69 @@ int main(int argc, char** argv) {
     int tag = 50;
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     MPI_Comm_size(MPI_COMM_WORLD, &totalProcesses);
-
+    
+    int M = 10;
+    int N = 10;
+    MPI_Status status;
+    int* array = malloc(N * sizeof(int));
     //producer
     if (myRank == 0) {
-        printf("Process %i producing value...\n", myRank);
-        int dataToSend = 500;
-        MPI_Send(&dataToSend, 1, MPI_INT, myRank + 1, tag, MPI_COMM_WORLD);
-    } else if (myRank < (totalProcesses - 1)) { //consumer-producer
-        int received;
+
+        int** matrix;
+        matrix = malloc(M * sizeof(int));
+        for (int i=0; i< M; i++) {
+            matrix[i] = malloc(N * sizeof(int)); 
+            for (int j=0; j< M; j++) {
+                matrix[i][j] = i * j;
+            }
+        }
+
+        printf("Process %i starting pipeline...\n", myRank);
+        
+        for (int i=0; i< M; i++) {
+            MPI_Send(matrix[i], N, MPI_INT, myRank + 1, tag, MPI_COMM_WORLD);
+        }
+
+    } else {
+
+        int receiveFrom = myRank - 1;
+        MPI_Recv(&array, N, MPI_INT, receiveFrom, tag, MPI_COMM_WORLD, &status);
+    }
+    
+    
+    //processa msg
+
+    if (myRank == totalProcesses - 1) //se for o ultimo
+    {
+        printf("Ultimo");
+    }
+    
+    
+     else { //consumer-producer
         int receiveFrom = myRank - 1;
         int sendTo = myRank + 1;
-        MPI_Status status;
-        MPI_Recv(&received, 1, MPI_INT, receiveFrom, tag, MPI_COMM_WORLD, &status);
-        printf("Process %i receiving from %i and passing value to %i ...\n", myRank, receiveFrom, sendTo);
-        MPI_Send(&received, 1, MPI_INT, sendTo, tag, MPI_COMM_WORLD);
+       
+        for (int i=0; i< M; i++) {
+            int* array = malloc(N * sizeof(int));
+            printf("Process %i receiving from %i and passing value to %i ...\n", myRank, receiveFrom, sendTo);
+            MPI_Recv(&array, N, MPI_INT, receiveFrom, tag, MPI_COMM_WORLD, &status);
+            MPI_Send(&array, N, MPI_INT, sendTo, tag, MPI_COMM_WORLD);
+        }
+  
     } else if (myRank == totalProcesses - 1) { //consumer
-        int received;
         int receiveFrom = myRank - 1;
-        MPI_Status status;
-        MPI_Recv(&received, 1, MPI_INT, receiveFrom, tag, MPI_COMM_WORLD, &status);
-        printf("FINISHED! Process %i received from %i from %i ...\n", myRank, received, receiveFrom);
+
+        for (int i=0; i< M; i++) {
+            int* array = malloc(N * sizeof(int));
+            MPI_Recv(&array, N, MPI_INT, receiveFrom, tag, MPI_COMM_WORLD, &status);
+
+            for (int j=0; j< N; j++) {
+               printf("Value at [%i][%i] = [%i]\n", i, j, array[j]);
+            }
+
+        }
+
+        printf("FINISHED!");
     }
 
     MPI_Finalize();
